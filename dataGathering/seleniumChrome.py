43f3942681fc55ecf4dafab2706c640e070ca3ca
unknown <milosh.p@live.com>
2017-06-27 11:48:59 +0200

    Changed selenium to go year by year

--- dataGathering/seleniumChrome.py
+++ dataGathering/seleniumChrome.py
@@ -26,8 +26,8 @@ def disable_stuff():
 if __name__ == '__main__':
 
     collected_data_location = "..\\data\\cars500.csv"
-    cars_number = 480
-    min_year = 2007
+    cars_number = 60
+    years = range(2007, 2018)
     links_visited_all = 0
 
     models = {
@@ -38,126 +38,128 @@ if __name__ == '__main__':
     num = re.compile(r'[^\d.]+')
     stopwatch_start = time()
 
+
     for make in models.keys():
         for model in models[make]:
+            for year in years:
+                # driver
+                driver = webdriver.Chrome(chrome_options=disable_stuff())
+                driver.get("https://www.mobile.de/?lang=en")
+                e = driver.find_element_by_id("qsdet")
+                e.click()
+                mainWindow = driver.current_window_handle
 
-            # driver
-            driver = webdriver.Chrome(chrome_options=disable_stuff())
-            driver.get("https://www.mobile.de/?lang=en")
-            e = driver.find_element_by_id("qsdet")
-            e.click()
-            mainWindow = driver.current_window_handle
+                # select make
+                sleep(0.5)
+                select = Select(driver.find_element_by_id("selectMake1-ds"))
+                select.select_by_visible_text(make)
 
-            # select make
-            sleep(0.5)
-            select = Select(driver.find_element_by_id("selectMake1-ds"))
-            select.select_by_visible_text(make)
+                # select year
+                Select(driver.find_element_by_id("minFirstRegistrationDate-s")).select_by_visible_text(str(year))
+                Select(driver.find_element_by_id("maxFirstRegistrationDate-s")).select_by_visible_text(str(year))
 
-            # select year
-            Select(driver.find_element_by_id("minFirstRegistrationDate-s")).select_by_visible_text(str(min_year))
+                # open file
+                carsFile = open(collected_data_location, "a")
 
-            # open file
-            carsFile = open(collected_data_location, "a")
+                driver.implicitly_wait(5)
 
-            driver.implicitly_wait(10)
+                # select model
+                Select(driver.find_element_by_id("selectModel1-ds")).select_by_visible_text(model)
+                driver.find_element_by_id("dsp-upper-search-btn").send_keys(Keys.CONTROL, Keys.RETURN)
 
-            # select model
-            Select(driver.find_element_by_id("selectModel1-ds")).select_by_visible_text(model)
-            driver.find_element_by_id("dsp-upper-search-btn").send_keys(Keys.CONTROL, Keys.RETURN)
+                tabs = driver.window_handles
+                driver.switch_to.window(tabs[-1])
+                searchWindow = driver.current_window_handle
 
-            tabs = driver.window_handles
-            driver.switch_to.window(tabs[-1])
-            searchWindow = driver.current_window_handle
-
-            # select sorting criteria
-            Select(driver.find_element_by_id("so-sb")).select_by_visible_text("Newest ads first")
-
-            try:
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, ".cBox.cBox--content.cBox--resultList>div>a")))
-            except TimeoutException:
-                continue
-
-            links_visited = 0
-
-            while links_visited < cars_number:
-
-                sleep(2)
-                car_links = driver.find_elements_by_css_selector('.cBox.cBox--content.cBox--resultList>div>a')
-                print("Car links size: " + str(len(car_links)))
-
-                for car_link in car_links:
-
-                    if links_visited % 20 == 0:
-                        print("Visited " + str(links_visited) + " links on the page in " + str(time() - stopwatch_start) + " s")
-                        print("Visited " + str(links_visited_all) + " links in " + str(time() - stopwatch_start) + " s")
-
-                    if links_visited > cars_number:
-                        break
-
-                    try:
-                        car_link.send_keys(Keys.CONTROL, Keys.RETURN)
-                    except StaleElementReferenceException:
-                        print("\t! Detached link")
-                        continue
-
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[-1])
-
-                    # collect car data
-
-                    car = Car()
-                    car.make = make
-                    car.model = model
-
-                    try:
-                        WebDriverWait(driver, 5).until(
-                            EC.presence_of_element_located((By.ID, "rbt-cubicCapacity-v")))
-                        WebDriverWait(driver, 5).until(
-                            EC.presence_of_element_located((By.ID, "rbt-mileage-v")))
-                        WebDriverWait(driver, 5).until(
-                            EC.presence_of_element_located((By.ID, "rbt-firstRegistration-v")))
-                        WebDriverWait(driver, 5).until(
-                            EC.presence_of_element_located((By.ID, "rbt-power-v")))
-                        WebDriverWait(driver, 5).until(
-                            EC.presence_of_element_located((By.CLASS_NAME, "rbt-prime-price")))
-                    except TimeoutException:
-                        print("\t! Car attributes exception")
-                        driver.close()
-                        driver.switch_to.window(searchWindow)
-                        continue
-
-                    car.ccm = num.sub('', driver.find_element_by_css_selector("#rbt-cubicCapacity-v").text)
-                    car.mileage = num.sub('', driver.find_element_by_css_selector("#rbt-mileage-v").text)
-                    car.year = (driver.find_element_by_css_selector("#rbt-firstRegistration-v")).text[3:]
-                    car.power = num.sub('', driver.find_element_by_css_selector("#rbt-power-v").text)
-                    car.price = num.sub('', driver.find_element_by_css_selector(".rbt-prime-price").text)
-
-                    carsFile.write(car.get_line() + "\n")
-                    # print(str(car) )
-
-                    driver.close()
-                    driver.switch_to.window(searchWindow)
-
-                    links_visited += 1
-                    links_visited_all += 1
+                # select sorting criteria
+                Select(driver.find_element_by_id("so-sb")).select_by_visible_text("Newest ads first")
 
                 try:
-                    nextPageButton = driver.find_element_by_css_selector("li.pref-next.u-valign-sub > span")
-                    href = nextPageButton.get_attribute("data-href")
-                    # print(href)
-                    driver.get(href)
-                except NoSuchElementException:
-                    break
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, ".cBox.cBox--content.cBox--resultList>div>a")))
+                except TimeoutException:
+                    continue
 
-            print("Visited links on the page: " + str(links_visited))
-            print("All visited links:         " + str(links_visited_all))
-            tabs = driver.window_handles
-            driver.switch_to.window(tabs[-1])
-            driver.close()
-            driver.switch_to.window(mainWindow)
+                links_visited = 0
 
-            carsFile.close()
-            driver.delete_all_cookies()
-            driver.quit()
-            sleep(10)
+                while links_visited < cars_number:
+
+                    sleep(2)
+                    car_links = driver.find_elements_by_css_selector('.cBox.cBox--content.cBox--resultList>div>a')
+                    print("Car links size: " + str(len(car_links)))
+
+                    for car_link in car_links:
+
+                        if links_visited % 20 == 0:
+                            print("Visited " + str(links_visited) + " links on the page in " + str(time() - stopwatch_start) + " s")
+                            print("Visited " + str(links_visited_all) + " links in " + str(time() - stopwatch_start) + " s")
+
+                        if links_visited > cars_number:
+                            break
+
+                        try:
+                            car_link.send_keys(Keys.CONTROL, Keys.RETURN)
+                        except StaleElementReferenceException:
+                            print("\t! Detached link")
+                            continue
+
+                        tabs = driver.window_handles
+                        driver.switch_to.window(tabs[-1])
+
+                        # collect car data
+
+                        car = Car()
+                        car.make = make
+                        car.model = model
+
+                        try:
+                            WebDriverWait(driver, 2).until(
+                                EC.presence_of_element_located((By.ID, "rbt-cubicCapacity-v")))
+                            WebDriverWait(driver, 2).until(
+                                EC.presence_of_element_located((By.ID, "rbt-mileage-v")))
+                            WebDriverWait(driver, 2).until(
+                                EC.presence_of_element_located((By.ID, "rbt-firstRegistration-v")))
+                            WebDriverWait(driver, 2).until(
+                                EC.presence_of_element_located((By.ID, "rbt-power-v")))
+                            WebDriverWait(driver, 2).until(
+                                EC.presence_of_element_located((By.CLASS_NAME, "rbt-prime-price")))
+                        except TimeoutException:
+                            print("\t! Car attributes exception")
+                            driver.close()
+                            driver.switch_to.window(searchWindow)
+                            continue
+
+                        car.ccm = num.sub('', driver.find_element_by_css_selector("#rbt-cubicCapacity-v").text)
+                        car.mileage = num.sub('', driver.find_element_by_css_selector("#rbt-mileage-v").text)
+                        car.year = (driver.find_element_by_css_selector("#rbt-firstRegistration-v")).text[3:]
+                        car.power = num.sub('', driver.find_element_by_css_selector("#rbt-power-v").text)
+                        car.price = num.sub('', driver.find_element_by_css_selector(".rbt-prime-price").text)
+
+                        carsFile.write(car.get_line() + "\n")
+                        # print(str(car) )
+
+                        driver.close()
+                        driver.switch_to.window(searchWindow)
+
+                        links_visited += 1
+                        links_visited_all += 1
+
+                    try:
+                        nextPageButton = driver.find_element_by_css_selector("li.pref-next.u-valign-sub > span")
+                        href = nextPageButton.get_attribute("data-href")
+                        # print(href)
+                        driver.get(href)
+                    except NoSuchElementException:
+                        break
+
+                print("Visited links on the page: " + str(links_visited))
+                print("All visited links:         " + str(links_visited_all))
+                tabs = driver.window_handles
+                driver.switch_to.window(tabs[-1])
+                driver.close()
+                driver.switch_to.window(mainWindow)
+
+                carsFile.close()
+                driver.delete_all_cookies()
+                driver.quit()
+                sleep(2)
--- fuzzy/fuzzySearch.py
+++ fuzzy/fuzzySearch.py
@@ -22,27 +22,27 @@ def type_function(x, c):
 
 def year_function(x, c):
     compared = np.arange(x, x + 1, 1)
-    val = fuzz.gbellmf(compared, 5, 3, c)
+    val = fuzz.sigmf(compared, c - 3, 1)
     return val
 
 
 def power_function(x, c):
-    compared = np.arange(x, x + 1, 1)
-    val = fuzz.gbellmf(compared, 5, 3, c)
+    compared = np.arange(x - 50, x + 50, 1)
+    val = fuzz.sigmf(compared, c - 20, 1)
+    plt.plot(compared, val)
+    plt.show()
     return val
 
 
 def ccm_function(x, c):
     compared = np.arange(x, x + 1, 1)
-    val = fuzz.gbellmf(compared, 5, 3, c)
+    val = fuzz.trimf(compared, [c-600, c, c+110])
     return val
 
 
 def mileage_function(x, c):
-    compared = np.arange(x, x + 100, 1)
-    val = fuzz.sigmf(compared, c + 25, -0.1)
-    plt.plot(compared, val, linewidth=2.0)
-    plt.show()
+    compared = np.arange(x, x + 1, 1)
+    val = fuzz.sigmf(compared, c + 25000, -1 / 10000)
     return val
 
 
@@ -58,8 +58,9 @@ def calculate(compared: Car, given: Car):
             year_function(compared.year, given.year) * \
             power_function(compared.power, given.power) * \
             ccm_function(compared.ccm, given.ccm) * \
-            mileage_function(compared.mileage/1000, given.mileage/1000) * \
+            mileage_function(compared.mileage, given.mileage) * \
             price_function(compared.price, given.price)
 
 if __name__ == '__main__':
-    print(mileage_function(170, 200))
+    # print(power_function())
+    print()
